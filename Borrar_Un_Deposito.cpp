837a06543f06456309b71cd66f37ded52d5b73a5
Diego González Hernández <diegobriaares@gmail.com>
2021-08-07 01:49:25 -0500

    Irwin, ahi te va

---
+++ Borrar_Un_Deposito.cpp
@@ -0,0 +1,52 @@
+/**
+
+   * Made by:    Diego Briaares
+   * At:   07.08.2021 00:48:42
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+	int main () {        
+		string date, amount;
+		cin >> date >> amount;
+		ifstream readD("Depositos.txt");
+		string d, a, b, c, xx, yy;
+		set<string> to_delete;
+		while (readD >> d) {
+			if (d == date) {
+				readD >> a;
+				if (a == amount) {
+					readD >> xx >> yy;
+					while (readD >> a) {
+						if ((int)a.size() > 15) {
+							to_delete.insert(a);
+							readD >> b >> c;
+						}
+						else break;
+					}
+					break;
+				}
+			}
+		}
+		ifstream readF("Facturas.txt");
+		stringstream outF;
+		int p = 0;
+		while (readF >> d) {
+			if (p % 4 == 0) {
+				if (to_delete.find(d) != to_delete.end()) {
+					readF >> a >> b >> c;
+					outF << d << " " << a << " 0 " << c << "\n";
+					to_delete.erase(to_delete.find(d));
+					continue;
+				}
+			}
+			outF << d;
+			p++;
+			if (p % 4 == 0) outF << "\n";
+			else outF << " ";
+		}
+		readF.close();
+		ofstream writeF("Facturas.txt");
+		writeF << outF.rdbuf(); 
+	}
